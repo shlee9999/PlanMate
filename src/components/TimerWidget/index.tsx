fb5240cc98c4f217b03d8c6735e11b896572eb87
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2023-05-10 22:45:57 +0900

    Feat: Remove Popup Feature

--- src/components/TimerWidget/index.tsx
+++ src/components/TimerWidget/index.tsx
@@ -4,47 +4,12 @@ import { useSelector } from "react-redux";
 import { useFormattedTime, startTimer, stopTimer } from "../../utils/helper";
 import { Globals } from "src/types";
 
-const width = 300;
-const height = 120;
-const left = window.screen.width - width;
-const top = 0;
-
 function TimerWidget({ title }) {
-  const [isPopupOpen, setIsPopupOpen] = useState(false);
   const isRunning = useSelector((state: Globals) => state.isRunning);
   const [time, setTime] = useState<number>(0);
   const intervalId = useRef<NodeJS.Timeout | null>(null);
   const formattedTime: string = useFormattedTime(time);
-  const popupWindow = useRef<Window | null>(null);
 
-  const handlePopupButtonClick = () => {
-    if (isPopupOpen) return;
-    const state = { key1: time.toString() };
-    openInNewWindow("/popup", state);
-    setIsPopupOpen(true);
-  };
-  function openInNewWindow(route: string, state: Record<string, string>): void {
-    const queryParams = new URLSearchParams(state).toString();
-    popupWindow.current = window.open(
-      `${route}?${queryParams}`,
-      "_blank",
-      `toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, copyhistory=no, 
-      width=${width}, height=${height}, top=${top}, left=${left}`
-    );
-    popupWindow.current?.focus();
-  }
-  useEffect(() => {
-    const handlePopupClosedMessage = (event: MessageEvent) => {
-      if (event.data.popupClosed) {
-        popupWindow.current = null;
-        setIsPopupOpen(false);
-      }
-    };
-    window.addEventListener("message", handlePopupClosedMessage);
-    return () => {
-      window.removeEventListener("message", handlePopupClosedMessage);
-    };
-  }, []);
   useEffect(() => {
     if (!isRunning) {
       stopTimer(intervalId.current);
@@ -52,14 +17,7 @@ function TimerWidget({ title }) {
     }
 
     intervalId.current = startTimer(() => {
-      setTime((prev) => {
-        const newState = prev + 1;
-        popupWindow.current?.postMessage(
-          { key1: newState },
-          window.location.origin
-        );
-        return newState;
-      });
+      setTime((prev) => prev + 1);
     });
   }, [isRunning]);
 
@@ -67,7 +25,6 @@ function TimerWidget({ title }) {
     <StyledTimerWidget>
       <p>{title}</p>
       <p>{formattedTime}</p>
-      <button onClick={handlePopupButtonClick}>Popup</button>
     </StyledTimerWidget>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -4,33 +4,16 @@ import { Provider } from "react-redux";
 import ReactDOM from "react-dom/client";
 import Main from "./page/Main/index";
 import { Globals, TodoItems } from "./types";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import TimerPopup from "./page/Popup/TimerPopup/index";
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Main />,
-  },
-  {
-    path: "/popup",
-    element: <TimerPopup />,
-  },
-]);
-function reducer(
-  state: Globals = { isRunning: false, todos: [] },
-  action
-): Globals {
+
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+
+function reducer(state: Globals = { isRunning: false, todos: [] }, action): Globals {
   switch (action.type) {
     case "ADD_TODO":
       console.log(state.todos);
       return { ...state, todos: [...state.todos, action.value] };
     case "DEL_TODO":
-      const newTodos = state.todos.filter(
-        (todo: TodoItems) => todo.id !== action.id
-      );
+      const newTodos = state.todos.filter((todo: TodoItems) => todo.id !== action.id);
       const newState = { ...state, todos: newTodos };
       return newState;
     case "RUN_STUDY":
@@ -52,7 +35,7 @@ const store = createStore<Globals, any, any, any>(reducer); //나중에 고치�
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router} />
+      <Main />
     </Provider>
   </React.StrictMode>
 );
