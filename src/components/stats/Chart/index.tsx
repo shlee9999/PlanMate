88319782ea4ebb5342e855373661bd4f3d0c6068
장 재 훈 <peng2412@naver.com>
2023-06-25 21:53:04 +0900

    BumpGraph add
    Library : Nivo.rocks
    
    수정 예정
    history : Nivo/stream
    now : Nivo/bump

--- src/components/stats/Chart/index.tsx
+++ src/components/stats/Chart/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { TimerCheckContainer } from './styled'
 import { PieGraph } from './component/PieGraph'
+import { BumpGraph } from './component/BumpGraph'
 
 function TimerCheck() {
   return (
     <TimerCheckContainer>
       <PieGraph/>
+      <BumpGraph/>
       {/* <TimerCheckGraph/> */}
     </TimerCheckContainer>
   )
