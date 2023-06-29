3c4529101b0bcaa39c4244a9a49b01f837b21fb1
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2023-06-29 19:06:15 +0900

    Fix: useTimer 오작동 해결

--- src/hooks/useTimer.tsx
+++ src/hooks/useTimer.tsx
@@ -9,6 +9,7 @@ export const useTimer = ({ defaultTime }: useTimerProps) => {
   const intervalRef = useRef<NodeJS.Timeout | null>(null)
 
   const startTimer = (): void => {
+    console.log(intervalRef.current)
     if (intervalRef.current) return
     intervalRef.current = setInterval(() => {
       setTime((prev) => prev + 1)
@@ -16,8 +17,10 @@ export const useTimer = ({ defaultTime }: useTimerProps) => {
   }
 
   const stopTimer = (): void => {
+    console.log(intervalRef.current)
     if (!intervalRef.current) return
     clearInterval(intervalRef.current)
+    intervalRef.current = null
   }
 
   const initializeTimer = (): void => {
