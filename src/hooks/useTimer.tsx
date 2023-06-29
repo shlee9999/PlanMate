86f8c4b0a373a21ef38247c4eff70e2c55a1373b
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2023-06-29 19:12:39 +0900

    Refactor: log 삭제

--- src/hooks/useTimer.tsx
+++ src/hooks/useTimer.tsx
@@ -9,7 +9,6 @@ export const useTimer = ({ defaultTime }: useTimerProps) => {
   const intervalRef = useRef<NodeJS.Timeout | null>(null)
 
   const startTimer = (): void => {
-    console.log(intervalRef.current)
     if (intervalRef.current) return
     intervalRef.current = setInterval(() => {
       setTime((prev) => prev + 1)
@@ -17,7 +16,6 @@ export const useTimer = ({ defaultTime }: useTimerProps) => {
   }
 
   const stopTimer = (): void => {
-    console.log(intervalRef.current)
     if (!intervalRef.current) return
     clearInterval(intervalRef.current)
     intervalRef.current = null
