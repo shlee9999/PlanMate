b59b921a72b2aa2229a1f070c22dbe49b6e684a1
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2023-06-29 17:04:20 +0900

    Design: HeaderSection

--- src/page/HeaderSection/index.tsx
+++ src/page/HeaderSection/index.tsx
@@ -1,8 +1,8 @@
 import { FC } from 'react'
-import { Logo, Root } from './styled'
+import { GreenTypo, GreetTypo, LeftContainer, Logo, Logout, Notice, RightContainer, Root, Tab } from './styled'
 import { tabList } from 'utils/helper'
-import { Tab } from 'page/MainSection/styled'
 import { useDispatch } from 'react-redux'
+import logo from 'assets/images/logo.png'
 
 type HeaderSectionProps = {}
 
@@ -10,19 +10,29 @@ export const HeaderSection: FC<HeaderSectionProps> = () => {
   const dispatch = useDispatch()
   return (
     <Root>
-      <Logo />
-      <Tab>
-        {tabList.map((item, index) => (
-          <div
-            key={index}
-            onClick={() => {
-              dispatch({ type: 'CHANGE_TAB', value: index })
-            }}
-          >
-            {item.title}
-          </div>
-        ))}
-      </Tab>
+      <LeftContainer>
+        <Logo src={logo} />
+        <Tab>
+          {tabList.map((item, index) => (
+            <div
+              key={index}
+              onClick={() => {
+                dispatch({ type: 'CHANGE_TAB', value: index })
+              }}
+            >
+              {item.title}
+            </div>
+          ))}
+        </Tab>
+      </LeftContainer>
+      <RightContainer>
+        <GreetTypo>
+          안녕하세요, <GreenTypo>메이트</GreenTypo>님!
+          {/* 닉네임으로 변경 */}
+        </GreetTypo>
+        <Logout>로그아웃</Logout>
+        <Notice>공지사항</Notice>
+      </RightContainer>
     </Root>
   )
 }
