5110d33848a2c01fbf11c0739dc0263215b71ec6
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2023-07-06 19:46:47 +0900

    Feat: Add routerInfo.tsx

---
+++ src/constants/routerInfo.tsx
@@ -0,0 +1,42 @@
+import { HeaderSection } from 'pages/CommonSections/HeaderSection'
+import { Outlet } from 'react-router-dom'
+import { Root } from 'styled'
+import { ExamInfoTab } from 'tabs/ExamInfoTab'
+import PlannerTab from 'tabs/PlannerTab'
+import StatsTab from 'tabs/StatsTab'
+import { TimerTab } from 'tabs/TimerTab'
+
+export const routerInfo = [
+  {
+    path: '/',
+    element: (
+      <Root>
+        <HeaderSection />
+        <Outlet />
+      </Root>
+    ),
+    children: [
+      {
+        path: 'timer',
+        element: <TimerTab />,
+      },
+      {
+        path: 'stats',
+        element: <StatsTab />,
+      },
+      {
+        path: 'planner',
+        element: <PlannerTab />,
+      },
+      {
+        path: 'examinfo',
+        element: <ExamInfoTab />,
+        // loader: () => {}, //비동기 처리 등
+      },
+      {
+        path: '*',
+        element: <>없는 페이지</>,
+      },
+    ],
+  },
+]
--- src/index.tsx
+++ src/index.tsx
@@ -2,52 +2,15 @@ import React from 'react'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import ReactDOM from 'react-dom/client'
-import { HeaderSection } from 'pages/CommonSections/HeaderSection'
 import rootReducer from 'modules'
-import { Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom'
-import { TimerTab } from 'tabs/TimerTab'
-import StatsTab from 'tabs/StatsTab'
-import PlannerTab from 'tabs/PlannerTab'
-import { ExamInfoTab } from 'tabs/ExamInfoTab'
-import { Root } from 'styled'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { routerInfo } from 'constants/routerInfo'
 
 const store = createStore(rootReducer)
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: (
-      <Root>
-        <HeaderSection />
-        <Outlet />
-      </Root>
-    ),
-    children: [
-      {
-        path: 'timer',
-        element: <TimerTab />,
-      },
-      {
-        path: 'stats',
-        element: <StatsTab />,
-      },
-      {
-        path: 'planner',
-        element: <PlannerTab />,
-      },
-      {
-        path: 'examinfo',
-        element: <ExamInfoTab />,
-        // loader: () => {}, //비동기 처리 등
-      },
-      {
-        path: '*',
-        element: <>없는 페이지</>,
-      },
-    ],
-  },
-])
+const router = createBrowserRouter(routerInfo)
+
 root.render(
   <Provider store={store}>
     <RouterProvider router={router} />
