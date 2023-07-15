cd57026929338ece733e6bd461b1f4d241c3cfa9
장 재 훈 <peng2412@naver.com>
2023-07-15 19:45:12 +0900

    rechart donut chart
    rechart donut chart

--- src/components/Stats/HistroyChart/component/StudyPieChart.tsx
+++ src/components/Stats/HistroyChart/component/StudyPieChart.tsx
@@ -1,71 +1,44 @@
-import React from 'react'
-import { PieChart, Pie, Cell, ResponsiveContainer } from 'recharts'
+import React, { PureComponent } from 'react'
+import { PieChart, Pie, Legend, Cell } from 'recharts'
 
-interface Data {
-  name: string
-  value: number
-}
-
-const data: Data[] = [
-  { name: 'Group A', value: 400 },
-  { name: 'Group B', value: 300 },
-  { name: 'Group C', value: 300 },
-  { name: 'Group D', value: 200 },
+const data = [
+  { name: '국어', value: 400, fill: '#0088FE' },
+  { name: '영어', value: 300, fill: '#00C49F' },
+  { name: '수학', value: 300, fill: '#FFBB28' },
+  { name: '과학', value: 200, fill: '#FF8042' },
 ]
 
-const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042']
-
-const RADIAN = Math.PI / 180
-const renderCustomizedLabel = ({
-  cx,
-  cy,
-  midAngle,
-  innerRadius,
-  outerRadius,
-  percent,
-  index,
-}: {
-  cx: number
-  cy: number
-  midAngle: number
-  innerRadius: number
-  outerRadius: number
-  percent: number
-  index: number
-}) => {
-  const radius = innerRadius + (outerRadius - innerRadius) * 0.5
-  const x = cx + radius * Math.cos(-midAngle * RADIAN)
-  const y = cy + radius * Math.sin(-midAngle * RADIAN)
-
-  return (
-    <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
-      {`${(percent * 100).toFixed(0)}%`}
-    </text>
-  )
+const renderColorfulLegendText = (value: string, entry: any) => {
+  return <span style={{ color: '#666666', fontWeight: 400, padding: '10px' }}>{value}</span>
 }
 
-const StudyPieChart: React.FC = () => {
-  return (
-    <ResponsiveContainer width="99%" aspect={1}>
+export default class StudyPieChart extends PureComponent {
+  render() {
+    return (
       <PieChart width={100} height={100}>
+        <Legend
+          height={13}
+          iconType="circle"
+          layout="vertical"
+          verticalAlign="middle"
+          iconSize={10}
+          formatter={renderColorfulLegendText}
+        />
         <Pie
           data={data}
-          cx="50%"
-          cy="50%"
-          labelLine={false}
-          label={renderCustomizedLabel}
-          innerRadius={40}
-          outerRadius={80}
+          cx={40}
+          cy={50}
+          innerRadius={20}
+          outerRadius={45}
           fill="#8884d8"
+          paddingAngle={0}
           dataKey="value"
         >
           {data.map((entry, index) => (
-            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+            <Cell key={`cell-${index}`} fill={entry.fill} />
           ))}
         </Pie>
       </PieChart>
-    </ResponsiveContainer>
-  )
+    )
+  }
 }
-
-export default StudyPieChart
