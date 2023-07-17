1c0c4b5f4747749bd834dd047ffd7640067ffb56
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2023-07-17 16:16:24 +0900

    Fix: draftjsToHTML 라이브러리 적용

--- src/utils/wysiwyg.ts
+++ src/utils/wysiwyg.ts
@@ -1,52 +1,10 @@
-import { convertFromRaw, convertToRaw, EditorState, RawDraftContentState } from 'draft-js'
-import { stateToHTML } from 'draft-js-export-html'
-
-const options = {
-  blockStyleFn: (block: any) => {
-    const type = block.getType()
-    // console.log(type)
-    if (type === 'block-type') {
-      return {
-        style: {
-          fontSize: '16px',
-          color: 'red',
-        },
-      }
-    }
-  },
-  inlineStyleFn: (styles: any) => {
-    const styleList: { [key: string]: string } = { color: 'color-', fontSize: 'fontsize-' }
-    const styleObject: any = {}
-    let isContainsKey = false
-
-    Object.keys(styleList).forEach((key) => {
-      styles.forEach((value: any) => {
-        if (value.startsWith(styleList[key])) {
-          isContainsKey = true
-          styleObject[key] = value.replace(styleList[key], '')
-          switch (key) {
-            case 'fontSize':
-              styleObject[key] += 'px'
-              break
-          }
-        }
-      })
-    })
-
-    if (isContainsKey) {
-      return {
-        element: 'span',
-        style: styleObject,
-      }
-    }
-  },
-}
+import { convertToRaw, EditorState } from 'draft-js'
+import draftToHtml from 'draftjs-to-html'
 
 export const deserializeContent = (jsonString: string): string => {
   try {
     const rawContent = JSON.parse(jsonString)
-    const contentState = convertFromRaw(rawContent)
-    return stateToHTML(contentState, options)
+    return draftToHtml(rawContent)
   } catch (e) {
     return jsonString
   }
