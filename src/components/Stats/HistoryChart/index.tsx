485009e037571db964d2ca75c49e7be6bc03edf9
장 재 훈 <peng2412@naver.com>
2023-07-17 19:28:24 +0900

    tsx 정리중
    tsx 정리중

---
+++ src/components/Stats/HistoryChart/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { ResultContainer, ResultStatsContainer, CompareStatsContainer } from './styled'
+import { MainHistory } from './component/MainHistory'
+import BumpGraph from '../CompareChart/component/BumpGraph'
+
+export const HistoryChart = () => {
+  return (
+    <ResultContainer>
+      <ResultStatsContainer>
+        <MainHistory />
+      </ResultStatsContainer>
+      <CompareStatsContainer>
+        <BumpGraph />
+      </CompareStatsContainer>
+    </ResultContainer>
+  )
+}
