f0f5fea7a6cd25cd6c1cdf6c58a61f7b1c3d940c
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2023-08-11 01:15:19 +0900

    Fix: D-Day 컴포넌트 api response 맞춰 변경

--- src/pages/MyPage/index.tsx
+++ src/pages/MyPage/index.tsx
@@ -39,26 +39,21 @@ import { findScrappedPost } from 'api/post/find/findScrappedPost'
 import { ResponseCommentType, ResponsePostType } from 'api/common/commonType'
 import { useSelector } from 'react-redux'
 import { RootState } from 'modules'
+import { FindAllScheduleResponseProps, findAllSchedule } from 'api/schedule/findAllSchedule'
 
 const myPageTabList = ['작성한 글', '작성한 댓글', '스크랩한 글']
 const sampleDDayList = [
-  {
-    title: '테스트1saasdsadfhjoisdfoasdjofjdsaofjdsoajsdfojdsoajo',
-    dDay: 30,
-    date: '2023-08-32',
-    isMarked: true,
-    dDayId: 0,
-  },
-  { title: '테스트2', dDay: 38, date: '2023-08-32', isMarked: true, dDayId: 1 },
-  { title: '테스트3', dDay: 50, date: '2023-08-32', isMarked: true, dDayId: 2 },
-  { title: '테스트4', dDay: 80, date: '2023-08-32', isMarked: false, dDayId: 3 },
-  { title: '테스트5', dDay: 90, date: '2023-08-32', isMarked: false, dDayId: 4 },
-  { title: '테스트6', dDay: 100, date: '2023-08-32', isMarked: false, dDayId: 5 },
-  { title: '테스트7', dDay: 105, date: '2023-08-32', isMarked: false, dDayId: 6 },
+  { id: 0, title: '테스트2', targetDate: '2023-08-20', isMarked: true },
+  { id: 1, title: '테스트3', targetDate: '2023-08-30', isMarked: true },
+  { id: 2, title: '테스트4', targetDate: '2023-09-20', isMarked: false },
+  { id: 3, title: '테스트5', targetDate: '2023-10-25', isMarked: false },
+  { id: 4, title: '테스트6', targetDate: '2023-11-30', isMarked: false },
+  { id: 5, title: '테스트7', targetDate: '2023-12-31', isMarked: false },
 ]
 
 export const MyPage: FC = () => {
   const userAuthInfo = useSelector((state: RootState) => state.userAuthInfo)
+  const [currentDDayList, setCurrentDDayList] = useState([])
   const [currentPage, setCurrentPage] = useState<number>(1)
   const [currentTab, setCurrentTab] = useState<string>(myPageTabList[0])
   const [isEllipsisModalOpen, setIsEllipsisModalOpen] = useState<boolean>(false)
@@ -137,6 +132,10 @@ export const MyPage: FC = () => {
         setCurrentPage(1)
       }
     })
+    findAllSchedule().then((res) => {
+      const response = res as FindAllScheduleResponseProps
+      setCurrentDDayList(response)
+    })
   }, [])
 
   useEffect(() => {
@@ -173,8 +172,8 @@ export const MyPage: FC = () => {
           </SeeMore>
         </TypoWrapper>
         <DDayContainer>
-          {sampleDDayList.map((dday) => (
-            <DDayItem {...dday} key={dday.dDayId} />
+          {currentDDayList.map((dday) => (
+            <DDayItem {...dday} key={dday.id} />
           ))}
 
           {/* <ArrowWrapper>
