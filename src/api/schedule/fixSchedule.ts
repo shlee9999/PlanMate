32b6b3dfc5eeca552e5a8c88cde5b3ca67013c67
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2023-08-15 16:53:22 +0900

    Fix: d day 관련 api 수정 - findFixedSchedule

--- src/api/schedule/fixSchedule.ts
+++ src/api/schedule/fixSchedule.ts
@@ -1,4 +1,4 @@
-import { axiosGET } from 'api/common/commonAxios'
+import { axiosPOST } from 'api/common/commonAxios'
 
 export type FixScheduleRequestProps = {
   id: number
@@ -7,5 +7,5 @@ export type FixScheduleRequestProps = {
 export type FixScheduleResponseProps = boolean
 
 export const fixSchedule = (req: FixScheduleRequestProps) => {
-  return axiosGET('/schedule/fix', req)
+  return axiosPOST('/schedule/fix', req)
 }
