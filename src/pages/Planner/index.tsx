e767029618f930306a71a7c1d00cd3e7e30daee7
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2024-01-08 18:41:16 +0900

    Remove: 일정추가 버튼 제거

--- src/pages/Planner/index.tsx
+++ src/pages/Planner/index.tsx
@@ -1,43 +1,9 @@
 //플래너 탭
-import { FC, useState } from 'react'
-import {
-  Root,
-  HeaderContainer,
-  HeaderContentWrapper,
-  HeaderMessage,
-  HeaderTitleLogo,
-  HeaderButton,
-  MainContainer,
-  MainWeeklyScheduler,
-} from './styled'
-import SubjectModal from 'components/Planner/SubjectModal'
+import { FC } from 'react'
+import { Root, HeaderContainer, HeaderContentWrapper, HeaderMessage, HeaderTitleLogo, MainContainer } from './styled'
 import { Scheduler } from 'components/Planner/Scheduler'
 
 export const PlannerPage: FC = () => {
-  const [isModalOpen, setIsModalOpen] = useState<boolean>(false)
-  const [modalTitle, setModalTitle] = useState<string>('')
-  const [mode, setMode] = useState<string>('study')
-
-  const openModal = (): void => {
-    const modeName = mode === 'study' ? '과목' : '종목'
-    setModalTitle(modeName + '추가')
-    setIsModalOpen(true)
-  }
-
-  const closeModal = (): void => {
-    setIsModalOpen(false)
-  }
-
-  const [isButtonHovered, setButtonHovered] = useState(false)
-
-  const handleMouseEnter = () => {
-    setButtonHovered(true)
-  }
-
-  const handleMouseLeave = () => {
-    setButtonHovered(false)
-  }
-
   return (
     <Root>
       <HeaderContainer>
@@ -45,21 +11,10 @@ export const PlannerPage: FC = () => {
           <HeaderMessage>안녕하세요!메이트 님!</HeaderMessage>
           <HeaderTitleLogo>플래너 🗓</HeaderTitleLogo>
         </HeaderContentWrapper>
-        <HeaderButton
-          isButtonHovered={isButtonHovered}
-          onMouseOver={handleMouseEnter}
-          onMouseOut={handleMouseLeave}
-          onClick={openModal}
-        >
-          +일정 추가
-        </HeaderButton>
       </HeaderContainer>
       <MainContainer>
-        <MainWeeklyScheduler>
-          <Scheduler />
-        </MainWeeklyScheduler>
+        <Scheduler />
       </MainContainer>
-      <SubjectModal title={modalTitle} isModalOpen={isModalOpen} closeModal={closeModal}></SubjectModal>
     </Root>
   )
 }
--- src/pages/Planner/styled.ts
+++ src/pages/Planner/styled.ts
@@ -40,45 +40,7 @@ export const HeaderTitleLogo = styled.p`
   margin-top: 2px;
 `
 
-interface Buttonprops {
-  isButtonHovered: boolean
-}
-
-export const HeaderButton = styled.button<Buttonprops>`
-  width: 96px;
-  height: 32px;
-  margin-left: 20px;
-  border: 1px solid #01cb45;
-  border-radius: 100px;
-  background-color: ${(props) => (props.isButtonHovered ? '#01CB45' : 'white')};
-  color: ${(props) => (props.isButtonHovered ? 'white' : '#01CB45')};
-`
-
 export const MainContainer = styled.div`
   display: flex;
   width: 100%;
-`
-
-export const MainWeeklyScheduler = styled.div``
-
-export const AddButton = styled.button`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  border-radius: 25px;
-  margin-top: 5px;
-  width: 64px;
-  height: 32px;
-
-  font-size: 14px;
-  font-weight: 400;
-  line-height: 32px;
-  color: #666666;
-  text-align: center;
-  cursor: pointer;
-`
-
-export const PlusImg = styled.img`
-  width: 15px;
-  height: 15px;
 `
