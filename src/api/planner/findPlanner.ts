1183e5eca693e08be67af99923775c45e706b033
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2024-01-13 11:41:51 +0900

    Fix: findPlanner props

--- src/api/planner/findPlanner.ts
+++ src/api/planner/findPlanner.ts
@@ -1,18 +1,14 @@
 import { axiosGET } from 'api/common/commonAxios'
 
-export type FindPlannerRequestProps = {
-  plannerId: number
-}
-
 export type FindPlannerResponseProps = {
   colorHex: string
   day: string
   endAt: string
   scheduleName: string
   startAt: string
-  plannerId: number
-}
+  type: string
+}[]
 
-export const findPlanner = (req: FindPlannerRequestProps) => {
-  return axiosGET('planner/find', req)
+export const findPlanner = () => {
+  return axiosGET('planner/find')
 }
