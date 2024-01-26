e7b7682410638b78389e7fa4776f10b362cf8f35
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2024-01-26 18:25:27 +0900

    Refactor: StudyTimeContainer

--- src/components/StatsContainer/components/StudyTimeContainer/StudyTimeContainer.tsx
+++ src/components/StatsContainer/components/StudyTimeContainer/StudyTimeContainer.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 import { StudyTime } from './StudyTime'
 import { TimeProps } from 'types'
 import { timeUtils } from 'utils'
-import * as s from './styled'
 import { StatsContainerType } from 'enums'
+import * as s from './styled'
 
 //총 공부시간, 최대 집중 시간
 
@@ -22,17 +22,25 @@ export const StudyTimeContainer: React.FC<StudyTimeContainerProps> = ({
   endAt,
 }) => {
   return (
-    <s.Root $type={type}>
-      {totalFocusTime && <StudyTime title="총 공부시간" text={timeUtils.getFormattedTime(totalFocusTime)} />}
-      {maxFocusTime && <StudyTime title="최대 집중 시간" text={timeUtils.getFormattedTime(maxFocusTime)} />}
-      <StudyTime
-        title="시작시간"
-        text={startAt.hour.toString().padStart(2, '0') + '시 ' + startAt.minute.toString().padStart(2, '0') + '분'}
-      />
-      <StudyTime
-        title="종료시간"
-        text={endAt.hour.toString().padStart(2, '0') + '시 ' + endAt.minute.toString().padStart(2, '0') + '분'}
-      />
-    </s.Root>
+    <s.StatsRoot $type={type}>
+      <s.StudyTimeRow>
+        {type === StatsContainerType.stats && (
+          <StudyTime title="총 공부시간" text={timeUtils.getFormattedTime(totalFocusTime)} />
+        )}
+        {type === StatsContainerType.stats && (
+          <StudyTime title="최대 집중 시간" text={timeUtils.getFormattedTime(maxFocusTime)} />
+        )}
+      </s.StudyTimeRow>
+      <s.StudyTimeRow>
+        <StudyTime
+          title="시작시간"
+          text={startAt.hour.toString().padStart(2, '0') + '시 ' + startAt.minute.toString().padStart(2, '0') + '분'}
+        />
+        <StudyTime
+          title="종료시간"
+          text={endAt.hour.toString().padStart(2, '0') + '시 ' + endAt.minute.toString().padStart(2, '0') + '분'}
+        />
+      </s.StudyTimeRow>
+    </s.StatsRoot>
   )
 }
--- src/components/StatsContainer/components/StudyTimeContainer/styled.ts
+++ src/components/StatsContainer/components/StudyTimeContainer/styled.ts
@@ -1,31 +1,36 @@
-import { H21_500, P12 } from 'commonStyled'
+import { FlexRow, H21_500, P12 } from 'commonStyled'
 import { StatsContainerType } from 'enums'
 import styled from 'styled-components'
 
 type RootProps = {
   $type: StatsContainerType
 }
-export const Root = styled.div<RootProps>`
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  grid-template-rows: 1fr 1fr;
-  column-gap: 20px;
+export const StatsRoot = styled.div<RootProps>`
+  width: 250px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  row-gap: 20px;
+  height: fit-content;
+  min-width: 0;
 `
-export const TimerBox = styled.div``
+export const StudyTimeRow = styled(FlexRow)`
+  &:first-child {
+    gap: 27px;
+  }
+  width: 100%;
+  gap: 20px;
+`
+export const TimerBox = styled.div`
+  white-space: nowrap;
+`
 
 export const Header = styled.div`
   ${P12}
-  width: 100%;
-  height: 18px;
   display: flex;
-  letter-spacing: 0em;
-  text-align: center;
   color: ${(props) => props.theme.text.gray1};
 `
 export const Time = styled.div`
   ${H21_500}
-  width: 100%;
-  height: 50px;
-  display: flex;
   color: ${(props) => props.theme.text.black1};
 `
