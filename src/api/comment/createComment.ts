8ffb69d73ad381dc75d4302040e45c6c8519c628
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2024-01-27 20:44:22 +0900

    Fix: useCreateCommentMutation 개선

--- src/api/comment/createComment.ts
+++ src/api/comment/createComment.ts
@@ -6,5 +6,5 @@ export type CreateCommentRequestProps = Pick<CommentType, 'content' | 'postId'>
 export type CreateCommentResponseProps = Pick<CommentType, 'commentId' | 'content' | 'memberName' | 'updatedAt'>
 
 /**댓글 생성 */
-export const createComment = ({ content, postId }: CreateCommentRequestProps) =>
+export const createComment = ({ content, postId }: CreateCommentRequestProps): Promise<CreateCommentResponseProps> =>
   axiosPOST('/comment/create', { content, postId })
--- src/pages/ExamInfo/hooks/mutations/comment/useCreateCommentMutation.ts
+++ src/pages/ExamInfo/hooks/mutations/comment/useCreateCommentMutation.ts
@@ -14,34 +14,43 @@ function useCreateCommentMutation() {
   const queryClient = useQueryClient()
   const { mutate } = useMutation(({ content, postId }: MutationProps) => createComment({ content, postId }), {
     onMutate: ({ isPostAuthor, currentPage, content, postId, callBack, memberName }) => {
-      const previousComments = queryClient.getQueryData(['commentData', postId, currentPage + ''])
+      const previousComments = queryClient.getQueryData<FindAllCommentsResponseProps>([
+        'commentData',
+        postId,
+        currentPage + '',
+      ])
+      const newComment = {
+        commentId: new Date().getTime(), //tempId
+        content,
+        isAuthor: true,
+        isMyHearted: false,
+        likeCount: 0,
+        memberName,
+        updatedAt: dateUtils.getKoreanISOString(new Date()).slice(0, 19),
+        postId,
+        isPostAuthor,
+      }
       queryClient.setQueryData<FindAllCommentsResponseProps>(['commentData', postId, currentPage + ''], (prev) => ({
         ...prev,
         totalCount: prev.totalCount + 1,
-        commentDtoList: [
-          {
-            commentId: new Date().getTime(), //tempId
-            content,
-            isAuthor: true,
-            isMyHearted: false,
-            likeCount: 0,
-            memberName,
-            updatedAt: dateUtils.getKoreanISOString(new Date()).slice(0, 19),
-            postId,
-            isPostAuthor,
-          },
-        ].concat(prev.commentDtoList),
+        commentDtoList: [newComment].concat(prev.commentDtoList),
       }))
       callBack()
       return { previousComments }
     },
-    onSuccess: (data, { postId, currentPage }) => {
-      // 성공 시 invalidate - commentId값 받아와야 수정 가능하므로)
-      queryClient.invalidateQueries(['commentData', postId, currentPage + ''])
+    onSuccess: (data, { postId, currentPage }, context) => {
+      queryClient.setQueryData<FindAllCommentsResponseProps>(['commentData', postId, currentPage + ''], (prev) => {
+        const updatedComment = prev.commentDtoList[0]
+        updatedComment.commentId = data.commentId
+        return { ...prev, commentDtoList: [updatedComment, ...context.previousComments.commentDtoList] }
+      })
+      console.log('create success')
     },
     onError: (err, { postId, currentPage }, context) => {
       // 오류 발생 시 원래 상태로 복원
       queryClient.setQueryData([['commentData', postId, currentPage + '']], context.previousComments)
+      console.error('create error')
+      console.error(err)
     },
   })
   return mutate
