d4387db4403a39a917aff3fee6cac6161372a7a4
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2024-01-27 22:56:54 +0900

    Refactor: constants/layout

--- src/constants/layout.ts
+++ src/constants/layout.ts
@@ -6,9 +6,7 @@ export const FOOTER_MAX_WIDTH = HEADER_MAX_WIDTH
 export const FOOTER_MIN_WIDTH = HEADER_MIN_WIDTH
 export const BODY_MAX_WIDTH = 1440
 export const BODY_MIN_WIDTH = 500
-export const DESKTOP_SIDE_MARGIN = 100
-export const TABLET_SIDE_MARGIN = 50
-export const MOBILE_SIDE_MARGIN = 20
+
 export const EXAMINFOITEM_MAX_WIDTH = 1088
 export const EXAMINFOITEM_MIN_WIDTH = 305
 export const TODOITEM_MAX_WIDTH = 1440
@@ -19,3 +17,7 @@ export const DDAYITEM_MIN_WIDTH = 120
 //* 반응형 레이아웃
 export const TABLET_SIZE = 1024
 export const MOBILE_SIZE = 768
+
+export const DESKTOP_SIDE_MARGIN = 100
+export const TABLET_SIDE_MARGIN = 50
+export const MOBILE_SIDE_MARGIN = 20
