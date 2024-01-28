3dd19c975bfc8015c9bc0670fcf029fc50518b4c
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2024-01-28 21:07:01 +0900

    Rename: DdayContainer, DdayItem

--- src/pages/MyPage/components/index.tsx
+++ src/pages/MyPage/components/index.tsx
@@ -1,6 +1,6 @@
-import { DdayItem } from './TempDdayItem/DdayItem'
+import { DdayItem } from './DdayItem/DdayItem'
 import { ProfileEditModal } from './ProfileEditModal/ProfileEditModal'
 import { ResignModal } from './ResignModal/ResignModal'
-import { DdayContainer } from './TempDdayContainer/DdayContainer'
+import { DdayContainer } from './DdayContainer/DdayContainer'
 
-export { DdayItem as DDayItem, ProfileEditModal, ResignModal, DdayContainer }
+export { DdayItem, ProfileEditModal, ResignModal, DdayContainer }
