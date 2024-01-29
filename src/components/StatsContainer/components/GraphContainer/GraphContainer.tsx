cf00a3f5acf7f34941086491a00b7f3483f1705a
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2024-01-29 15:07:00 +0900

    Design: TimerStatsContainer

--- src/components/StatsContainer/components/GraphContainer/GraphContainer.tsx
+++ src/components/StatsContainer/components/GraphContainer/GraphContainer.tsx
@@ -10,11 +10,11 @@ type GraphContainerProps = {
   type: StatsContainerType
 }
 export const GraphContainer = ({ type }: GraphContainerProps) => {
-  return (
-    <s.Root>
-      <s.TypoContainer>
+  return type === StatsContainerType.stats ? (
+    <s.StatsGraphContainer>
+      <s.StatsTypoContainer>
         <CompareTitle>오후 7시까지의 비교</CompareTitle>
-        <s.CompareTimer>+ 00:30:13</s.CompareTimer>
+        <s.StatsCompareTimer>+ 00:30:13</s.StatsCompareTimer>
         <s.LegendContainer>
           <s.Container>
             <s.LegendTitle>오늘 </s.LegendTitle>
@@ -25,10 +25,30 @@ export const GraphContainer = ({ type }: GraphContainerProps) => {
             <s.IconYesterdayDot alt="yesterday_dot" src={yesterdayDotImg} />
           </s.Container>
         </s.LegendContainer>
-      </s.TypoContainer>
-      <s.StatsGraphWrapper $type={type}>
+      </s.StatsTypoContainer>
+      <s.StatsGraphWrapper>
         <BumpGraph />
       </s.StatsGraphWrapper>
-    </s.Root>
+    </s.StatsGraphContainer>
+  ) : (
+    <s.TimerGraphContainer>
+      <s.TimerTypoContainer>
+        <CompareTitle>오후 7시까지의 비교</CompareTitle>
+        <s.TimerCompareTimer>+ 00:30:13</s.TimerCompareTimer>
+        <s.LegendContainer>
+          <s.Container>
+            <s.LegendTitle>오늘 </s.LegendTitle>
+            <s.IconTodayDot alt="today_dot" src={todayDotImg} />
+          </s.Container>
+          <s.Container>
+            <s.LegendTitle>어제</s.LegendTitle>
+            <s.IconYesterdayDot alt="yesterday_dot" src={yesterdayDotImg} />
+          </s.Container>
+        </s.LegendContainer>
+      </s.TimerTypoContainer>
+      <s.TimerGraphWrapper>
+        <BumpGraph />
+      </s.TimerGraphWrapper>
+    </s.TimerGraphContainer>
   )
 }
