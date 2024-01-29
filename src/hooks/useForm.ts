d6ab9b0fad3ba0d0ee0ffe55b0e29f4d7de79e50
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2024-01-29 19:52:37 +0900

    Feat: useForm input, textarea 분리 및 focus 추가

--- src/hooks/useForm.ts
+++ src/hooks/useForm.ts
@@ -1,16 +1,17 @@
-import { useState } from 'react'
+import React, { LegacyRef } from 'react'
+import { useState, useRef } from 'react'
 
 type RegisterProps = {
   maxLength?: number
+  isTextArea?: boolean
 }
 
 const DEFAULT_MAX_LENGTH = 10
-/**
- *
- * @returns register, handleSubmit
- */
+
 function useForm<T extends { [key: string]: string }>() {
   const [values, setValues] = useState<T>({} as T)
+  const inputRefs = useRef<{ [key in keyof T]?: React.RefObject<HTMLInputElement> }>({})
+  const textareaRefs = useRef<{ [key in keyof T]?: React.RefObject<HTMLTextAreaElement> }>({})
 
   const handleSubmit = (userCallback: (data: T) => void) => {
     return (e: React.FormEvent<HTMLFormElement>) => {
@@ -21,21 +22,54 @@ function useForm<T extends { [key: string]: string }>() {
       userCallback(values)
     }
   }
-
-  const register = (key: string, { maxLength = DEFAULT_MAX_LENGTH }: RegisterProps = {}) => {
-    const handleChange = (e) => {
+  const registerInput = (key: keyof T, { maxLength = DEFAULT_MAX_LENGTH }: RegisterProps = {}) => {
+    if (!inputRefs.current[key]) {
+      inputRefs.current[key] = React.createRef<HTMLInputElement>()
+    }
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
       const newValue = e.target.value
       if (newValue.length <= maxLength) setValues((prev) => ({ ...prev, [key]: newValue }))
     }
     return {
+      ref: inputRefs.current[key] as React.RefObject<HTMLInputElement>,
       value: values[key] || '',
       onChange: handleChange,
     }
   }
-  const setValue = ({ key, value }: { key: keyof T; value: string }) => {
+  const registerTextarea = (key: keyof T, { maxLength = DEFAULT_MAX_LENGTH }: RegisterProps = {}) => {
+    if (!textareaRefs.current[key]) {
+      textareaRefs.current[key] = React.createRef<HTMLTextAreaElement>()
+    }
+    const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+      console.log(e.target.value)
+      const newValue = e.target.value
+      if (newValue.length <= maxLength) setValues((prev) => ({ ...prev, [key]: newValue }))
+    }
+    return {
+      ref: textareaRefs.current[key] as React.RefObject<HTMLTextAreaElement>,
+      value: values[key] || '',
+      onChange: handleChange,
+    }
+  }
+
+  const setValue = (key: keyof T, value: string) => {
     setValues((prev) => ({ ...prev, [key]: value }))
   }
-  return { register, handleSubmit, setValue }
+
+  const inputFocus = (key: keyof T) => {
+    const ref = inputRefs.current[key]
+    if (ref && ref.current) {
+      ref.current.focus()
+    }
+  }
+  const textareaFocus = (key: keyof T) => {
+    const ref = textareaRefs.current[key]
+    if (ref && ref.current) {
+      ref.current.focus()
+    }
+  }
+
+  return { registerInput, registerTextarea, handleSubmit, setValue, inputFocus, textareaFocus }
 }
 
 export default useForm
