f71561165c499d41ebc4dba5ad81d48aef6fa4a4
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2024-01-31 14:10:32 +0900

    Refactor: DdayCalendarPage

--- src/pages/MyPage/DdayCalendarPage/DdayCalendarPage.tsx
+++ src/pages/MyPage/DdayCalendarPage/DdayCalendarPage.tsx
@@ -1,65 +1,29 @@
 import * as s from './styled'
-import React, { FC, useEffect, useState } from 'react'
-import { dateUtils, formatTwoDigits } from 'utils'
-import { useNavigate } from 'react-router-dom'
-import { useQuery } from 'react-query'
-import { FindAllDdayResponseProps, findAllDday } from 'api/dday/findAllDday'
-import { useAddDdayMutation, useEditDdayMutation, useDeleteDdayMutation } from '../hooks'
+import React, { FC } from 'react'
 import { MAX_DDAY_CHARACTER_COUNT } from 'constants/maxCharacterCount'
-import { QueryKeys } from 'types'
-import { useForm } from 'hooks'
+import { useDdayCalendarPage } from './useDdayCalendarPage'
+import { formatTwoDigits } from 'utils'
 
 type DdayCalendarProps = {
   className?: string
 }
-type IForm = {
-  dDayTitle: string
-}
+
 export const DdayCalendarPage: FC<DdayCalendarProps> = ({ className }) => {
-  const [isEditing, setIsEditing] = useState(false)
-  const [selectedDDayId, setSelectedDDayId] = useState(-1)
-  const [selectedDateProps, setSelectedDateProps] = useState(dateUtils.getTodayDateProps())
-  const { registerInput, handleSubmit, setValue, inputFocus } = useForm<IForm>()
-  const setDdayTitle = (title: string) => setValue('dDayTitle', title)
-  const { data: dDayList, isLoading } = useQuery<FindAllDdayResponseProps>([QueryKeys.dDayList], () => findAllDday())
-  const navigate = useNavigate()
-  const mutateAddSchedule = useAddDdayMutation()
-  const mutateEditSchedule = useEditDdayMutation()
-  const mutateDeleteSchedule = useDeleteDdayMutation()
-  const onClickDelete = (e: React.MouseEvent) => {
-    e.preventDefault() // * submit 방지
-    mutateDeleteSchedule({ dDayId: selectedDDayId, callBack: () => setIsEditing(false) })
-  }
-  const onSubmit = ({ dDayTitle }: IForm) => {
-    if (!dateUtils.isTodayOrFuture(selectedDateProps)) return
-    //* 과거 시간은 추가 수정 X
-    if (!isEditing) {
-      //* 추가
-      mutateAddSchedule({
-        targetDate: dateUtils.getYYYYMMDD(selectedDateProps),
-        title: dDayTitle,
-        callBack: () => {
-          setDdayTitle('')
-          setSelectedDateProps(dateUtils.getDateProps(new Date()))
-        },
-      })
-    } else {
-      //* 수정
-      mutateEditSchedule({
-        targetDate: dateUtils.getYYYYMMDD(selectedDateProps),
-        title: dDayTitle,
-        dDayId: selectedDDayId,
-        callBack: () => {
-          setDdayTitle('')
-        },
-      })
-    }
-  }
-
-  useEffect(() => {
-    inputFocus('dDayTitle')
-  }, [selectedDateProps])
-
+  const {
+    dDayList,
+    setSelectedDateProps,
+    setDdayTitle,
+    setIsEditing,
+    setSelectedDDayId,
+    isLoading,
+    isEditing,
+    handleSubmit,
+    registerInput,
+    selectedDateProps,
+    onClickDelete,
+    onClickBackButton,
+    onSubmit,
+  } = useDdayCalendarPage()
   return (
     <s.DdayCalendarPage className={className}>
       <s.MainContainer>
@@ -76,7 +40,7 @@ export const DdayCalendarPage: FC<DdayCalendarProps> = ({ className }) => {
             isDDayLoading={isLoading}
             isEditing={isEditing}
           >
-            <s.BackButton onClick={() => navigate(-1)} />
+            <s.BackButton onClick={onClickBackButton} />
           </s.StyledDDayContainer>
           <s.AddDdayBox $isEditing={isEditing} title={`D-DAY ${isEditing ? '수정' : '추가'}`} right>
             <s.Form onSubmit={handleSubmit(onSubmit)}>
---
+++ src/pages/MyPage/DdayCalendarPage/useDdayCalendarPage.ts
@@ -0,0 +1,73 @@
+import { FindAllDdayResponseProps, findAllDday } from 'api/dday/findAllDday'
+import { useForm } from 'hooks'
+import { useState, useEffect } from 'react'
+import { useQuery } from 'react-query'
+import { useNavigate } from 'react-router-dom'
+import { QueryKeys } from 'types'
+import { dateUtils } from 'utils'
+import { useAddDdayMutation, useEditDdayMutation, useDeleteDdayMutation } from '../hooks'
+
+type IForm = {
+  dDayTitle: string
+}
+export const useDdayCalendarPage = () => {
+  const [isEditing, setIsEditing] = useState(false)
+  const [selectedDDayId, setSelectedDDayId] = useState(-1)
+  const [selectedDateProps, setSelectedDateProps] = useState(dateUtils.getTodayDateProps())
+  const { registerInput, handleSubmit, setValue, inputFocus } = useForm<IForm>()
+  const setDdayTitle = (title: string) => setValue('dDayTitle', title)
+  const { data: dDayList, isLoading } = useQuery<FindAllDdayResponseProps>([QueryKeys.dDayList], () => findAllDday())
+  const navigate = useNavigate()
+  const mutateAddSchedule = useAddDdayMutation()
+  const mutateEditSchedule = useEditDdayMutation()
+  const mutateDeleteSchedule = useDeleteDdayMutation()
+  const onClickDelete = (e: React.MouseEvent) => {
+    e.preventDefault() // * submit 방지
+    mutateDeleteSchedule({ dDayId: selectedDDayId, callBack: () => setIsEditing(false) })
+  }
+  const onClickBackButton = () => navigate(-1)
+  const onSubmit = ({ dDayTitle }: IForm) => {
+    if (!dateUtils.isTodayOrFuture(selectedDateProps)) return
+    //* 과거 시간은 추가 수정 X
+    if (!isEditing) {
+      //* 추가
+      mutateAddSchedule({
+        targetDate: dateUtils.getYYYYMMDD(selectedDateProps),
+        title: dDayTitle,
+        callBack: () => {
+          setDdayTitle('')
+          setSelectedDateProps(dateUtils.getDateProps(new Date()))
+        },
+      })
+    } else {
+      //* 수정
+      mutateEditSchedule({
+        targetDate: dateUtils.getYYYYMMDD(selectedDateProps),
+        title: dDayTitle,
+        dDayId: selectedDDayId,
+        callBack: () => {
+          setDdayTitle('')
+        },
+      })
+    }
+  }
+  useEffect(() => {
+    inputFocus('dDayTitle')
+  }, [selectedDateProps])
+
+  return {
+    dDayList,
+    setSelectedDateProps,
+    setDdayTitle,
+    setIsEditing,
+    setSelectedDDayId,
+    isLoading,
+    isEditing,
+    handleSubmit,
+    registerInput,
+    selectedDateProps,
+    onClickDelete,
+    onClickBackButton,
+    onSubmit,
+  }
+}
