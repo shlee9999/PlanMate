9a4c85f71283a01558fa33646361ff117daf6a77
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2024-02-04 17:09:20 +0900

    Feat: CalendarHeader memo 적용

--- src/components/Calendar/components/CalendarHeader/CalendarHeader.tsx
+++ src/components/Calendar/components/CalendarHeader/CalendarHeader.tsx
@@ -1,5 +1,5 @@
 import * as s from './styled'
-import { Dispatch, FC, SetStateAction } from 'react'
+import { Dispatch, SetStateAction, memo } from 'react'
 import { DateProps } from 'types'
 import { useCalendarHeader } from './useCalendarHeader'
 
@@ -13,46 +13,48 @@ type CalendarHeaderProps = {
   setBack: Dispatch<SetStateAction<boolean>>
 }
 
-export const CalendarHeader: FC<CalendarHeaderProps> = ({
-  yearHeader,
-  todayButton,
-  selectedDateProps,
-  headerButtonLayout,
-  blockFuture,
-  setSelectedDate,
-  setBack,
-}) => {
-  const { onClickNextMonth, onClickPrevMonth, onClickNextYear, onClickPrevYear, onClickToday, todayDateProps } =
-    useCalendarHeader({
-      setSelectedDate,
-      blockFuture,
-      selectedDateProps,
-      setBack,
-    })
-  return (
-    <>
-      {yearHeader && (
-        <s.YearHeader>
-          <s.PrevButton onClick={onClickPrevYear} />
-          <s.Month $layout={headerButtonLayout} key={selectedDateProps.month}>
-            {selectedDateProps.year}
-          </s.Month>
-          <s.NextButton onClick={onClickNextYear} />
-          {todayButton && <s.TodayButton onClick={onClickToday}>Today</s.TodayButton>}
-        </s.YearHeader>
-      )}
-      <s.MonthHeader $layout={headerButtonLayout}>
-        <s.PrevButton onClick={onClickPrevMonth} />
-        <s.Month $layout={headerButtonLayout} key={selectedDateProps.month}>
-          {selectedDateProps.month}월
-        </s.Month>
-        {(!blockFuture ||
-          selectedDateProps.year < new Date().getFullYear() ||
-          (selectedDateProps.year === new Date().getFullYear() && selectedDateProps.month < todayDateProps.month)) && (
-          <s.NextButton onClick={onClickNextMonth} />
+export const CalendarHeader = memo(
+  ({
+    yearHeader,
+    todayButton,
+    selectedDateProps,
+    headerButtonLayout,
+    blockFuture,
+    setSelectedDate,
+    setBack,
+  }: CalendarHeaderProps) => {
+    const { onClickNextMonth, onClickPrevMonth, onClickNextYear, onClickPrevYear, onClickToday, todayDateProps } =
+      useCalendarHeader({
+        setSelectedDate,
+        blockFuture,
+        selectedDateProps,
+        setBack,
+      })
+    return (
+      <>
+        {yearHeader && (
+          <s.YearHeader>
+            <s.PrevButton onClick={onClickPrevYear} />
+            <s.Month $layout={headerButtonLayout} key={selectedDateProps.month}>
+              {selectedDateProps.year}
+            </s.Month>
+            <s.NextButton onClick={onClickNextYear} />
+            {todayButton && <s.TodayButton onClick={onClickToday}>Today</s.TodayButton>}
+          </s.YearHeader>
         )}
-        {todayButton && <s.TodayButton onClick={onClickToday}>Today</s.TodayButton>}
-      </s.MonthHeader>
-    </>
-  )
-}
+        <s.MonthHeader $layout={headerButtonLayout}>
+          <s.PrevButton onClick={onClickPrevMonth} />
+          <s.Month $layout={headerButtonLayout} key={selectedDateProps.month}>
+            {selectedDateProps.month}월
+          </s.Month>
+          {(!blockFuture ||
+            selectedDateProps.year < new Date().getFullYear() ||
+            (selectedDateProps.year === new Date().getFullYear() &&
+              selectedDateProps.month < todayDateProps.month)) && <s.NextButton onClick={onClickNextMonth} />}
+          {todayButton && <s.TodayButton onClick={onClickToday}>Today</s.TodayButton>}
+        </s.MonthHeader>
+      </>
+    )
+  }
+)
+CalendarHeader.displayName = 'CalendarHeader'
