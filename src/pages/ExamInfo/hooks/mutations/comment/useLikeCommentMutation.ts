ca58dfde93b6c24dca43f8c630c0c8e291e0c482
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2024-02-14 04:19:22 +0900

    Feat: MyPage에서 Comment 좋아요 가능

--- src/pages/ExamInfo/hooks/mutations/comment/useLikeCommentMutation.ts
+++ src/pages/ExamInfo/hooks/mutations/comment/useLikeCommentMutation.ts
@@ -3,6 +3,8 @@ import { likeComment } from 'api/comment/likeComment'
 import { CommentType } from 'api/types'
 import { QueryKeys } from 'types'
 import { useMutation, useQueryClient } from 'react-query'
+import { useMatch } from 'react-router-dom'
+import { FindCommentResponseProps } from 'api/comment/findComment'
 
 type MutationProps = Pick<CommentType, 'commentId' | 'postId' | 'currentPage'>
 
@@ -13,13 +15,13 @@ type MutationProps = Pick<CommentType, 'commentId' | 'postId' | 'currentPage'>
  */
 /**댓글 좋아요*/
 function useLikeCommentMutation() {
+  const isMyPage = useMatch('/mypage')
   const queryClient = useQueryClient()
   const { mutate } = useMutation(({ commentId }: MutationProps) => likeComment({ commentId }), {
     onMutate: ({ commentId, postId, currentPage }: MutationProps) => {
-      const previousData = queryClient.getQueryData([QueryKeys.commentData, postId, currentPage + ''])
-      queryClient.setQueryData<FindAllCommentsResponseProps>(
-        [QueryKeys.commentData, postId, currentPage + ''],
-        (prev) => ({
+      if (isMyPage) {
+        const previousData = queryClient.getQueryData([QueryKeys.myCommentInfo, currentPage])
+        queryClient.setQueryData<FindCommentResponseProps>([QueryKeys.myCommentInfo, currentPage], (prev) => ({
           ...prev,
           commentDtoList: prev.commentDtoList.map((prevComment) =>
             prevComment.commentId === commentId
@@ -30,9 +32,28 @@ function useLikeCommentMutation() {
                 }
               : prevComment
           ),
-        })
-      )
-      return { previousData }
+        }))
+        return { previousData }
+      } else {
+        // Detail Page
+        const previousData = queryClient.getQueryData([QueryKeys.commentData, postId, currentPage + ''])
+        queryClient.setQueryData<FindAllCommentsResponseProps>(
+          [QueryKeys.commentData, postId, currentPage + ''],
+          (prev) => ({
+            ...prev,
+            commentDtoList: prev.commentDtoList.map((prevComment) =>
+              prevComment.commentId === commentId
+                ? {
+                    ...prevComment,
+                    isMyHearted: !prevComment.isMyHearted,
+                    likeCount: prevComment.isMyHearted ? prevComment.likeCount - 1 : prevComment.likeCount + 1,
+                  }
+                : prevComment
+            ),
+          })
+        )
+        return { previousData }
+      }
     },
     onError: (err, { postId, currentPage }, context) => {
       console.error(err)
