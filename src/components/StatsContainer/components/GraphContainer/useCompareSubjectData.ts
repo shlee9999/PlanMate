09119e3eff1763fe28686a74bf069ba0dd647d20
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2024-02-16 20:56:51 +0900

    Feat: 차트 현재시간 적용

--- src/components/StatsContainer/components/GraphContainer/useCompareSubjectData.ts
+++ src/components/StatsContainer/components/GraphContainer/useCompareSubjectData.ts
@@ -3,16 +3,15 @@ import { useTodayStats } from 'pages/Timer/hooks'
 import { useQuery } from 'react-query'
 import { QueryKeys } from 'types'
 import { timeUtils } from 'utils'
-interface CompareSubjectDataType {
-  hour: string
-  today: number
-  yesterday: number
-  lastDot?: number
-}
 
 const compareHours = ['00:00', '06:00', '12:00', '18:00', '24:00']
-
+const sortByTime = (a, b) => {
+  const [hoursA, minutesA] = a.hour.split(':').map((n) => parseInt(n, 10))
+  const [hoursB, minutesB] = b.hour.split(':').map((n) => parseInt(n, 10))
+  return hoursA - hoursB || minutesA - minutesB
+}
 export const useCompareSubjectData = () => {
+  const currentTimeFormatted = timeUtils.getCurrentTime().slice(0, 5) // 이 함수가 'HH:mm' 형식의 문자열을 반환한다고 가정합니다.
   const { data: compareSubjectData, isLoading: isCompareSubjectDataLoading } = useQuery<CompareSubjectResponseProps>(
     [QueryKeys.compareData],
     () => compareSubject(),
@@ -69,18 +68,40 @@ export const useCompareSubjectData = () => {
       },
     }
   )
-  const { nowGraphHour, yesterdayStudyTimeList } = compareSubjectData
+  const { yesterdayStudyTimeList, todayStudyTimeList } = compareSubjectData
+  const yesterdayStudyTime = yesterdayStudyTimeList.map((timeData) => timeUtils.timeToSecond(timeData))
+  const todayStudyTime = todayStudyTimeList.map((timeData) => timeUtils.timeToSecond(timeData))
   const { totalStudyTime: todayStudyTimeProps } = useTodayStats()
-  const todayStudyTime = timeUtils.timeToSecond(todayStudyTimeProps)
-  const yesterdayStudyTime = yesterdayStudyTimeList.map((yesterdayStudyTime) =>
-    timeUtils.timeToSecond(yesterdayStudyTime)
-  )
-
-  const parsedData: CompareSubjectDataType[] = compareHours.map((hour, index) => ({
-    hour,
-    today: index == 0 ? 0 : todayStudyTime, //todo: 배열로바꾸기
-    yesterday: index == 0 ? 0 : yesterdayStudyTime[index - 1],
-    lastDot: index === 0 ? undefined : +hour.slice(0, 2) === nowGraphHour ? todayStudyTime : undefined, //todo: 현재시간으로 표시해주기
-  }))
+  if (!compareHours.includes(currentTimeFormatted)) {
+    compareHours.push(currentTimeFormatted)
+  }
+  const totalStudyTime = timeUtils.timeToSecond(todayStudyTimeProps)
+  const parsedData = compareHours
+    .map((hour, index) => {
+      if (index === 0)
+        return {
+          hour,
+          today: 0,
+          yesterday: 0,
+          lastDot: undefined,
+        }
+      const todayTime = index < 5 ? todayStudyTime[index - 1] : totalStudyTime
+      const yesterdayTime = index < 5 ? yesterdayStudyTime[index - 1] : 0
+      return {
+        hour,
+        today: todayTime,
+        yesterday: yesterdayTime,
+        lastDot: hour === currentTimeFormatted ? todayTime : undefined,
+      }
+    })
+    .sort(sortByTime)
+    .map((data, index, arr) => {
+      if (data.hour === currentTimeFormatted && arr[index - 1] && arr[index + 1]) {
+        data.yesterday = (arr[index - 1].yesterday + arr[index + 1].yesterday) / 2
+      }
+      if (+data.hour.slice(0, 2) > +currentTimeFormatted.slice(0, 2)) data.today = undefined
+      return data
+    })
+  console.log(parsedData)
   return { parsedData, isCompareSubjectDataLoading }
 }
